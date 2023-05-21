d635c95e3ca48dbd326c7a66dc967191bfe2181f
Nirantarr <nirantar.m844@gmail.com>
2023-05-21 22:44:35 +0530

    Add files via upload
    Express.js Concepts

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-const http = require('http');
 const fs = require('fs');
-
 const data = JSON.parse(fs.readFileSync('data.json','utf-8')); // json is read
 const index = fs.readFileSync('index.html','utf-8'); // index.html is read
-const product = data.carts; // json data is fetched
-const server = http.createServer((req,res)=>{
 
-    if((req.url).startsWith('/product')){ //used javascirpt to get /product url
-        const id = req.url.split('/')[2]; // we get the /product/1 
-        // console.log(id);
-        const prd = product.find(p=>p.id===(+id)); // we find the received id in data json file.
-        // console.log(prd);
-        res.setHeader("Content-Type","text/html");
-        let modifiedIndex = index.replace('**50**',prd.userId).replace("**10**",prd.totalQuantity).replace('**20**',prd.total) // we replaced the content with values
-        res.end(modifiedIndex);
-        return;
-    }
+const express = require('express');
+const server = express();
 
-    switch(req.url){ //these are cases for server side rendering
-        case '/':
-            res.setHeader("Content-Type","text/html");
-            let modifiedIndex = index.replace('**50**',product.userId).replace("**10**",product.totalQuantity).replace('**20**',product.total)
-            res.end(modifiedIndex);
-            break;
-    
-        case '/json':
-            res.setHeader("Content-Type","application/json");
-            res.end(JSON.stringify(data));
-            break;
-    
-        default :
-            res.writeHead(404);
-            res.end();
-    }
-//  console.log("server started");
-//  res.setHeader("Content-Type","application/json")
-//  res.end(JSON.stringify(data));
+// STATIC Middleware
+server.use(express.static('public')); // with this files under the public folder will get hosted.
+
+server.get('/demo',(req,res)=>{
+// res.send("Hello world");
+// res.sendFile('\Coding Stuff\node learn\index.html');
+res.sendStatus(202);
 })
-server.listen(8080);
+
+// USING MIDDLEWARE 
+// middleware are those which are in between user and server. If user sents req before getting response it must go underthrough middleware.
+// APPLICATION MIDDLEWARE
+// server.use((req,res,next)=>{
+//  console.log(req.method,req.ip,req.hostname);
+//  next();
+// })
+
+// ROUTE-LEVEL-MIDDLEWARE
+const auth = (req,res,next)=>{
+    console.log(req.query);
+    if(req.query.password==123){
+        next();
+    }else{
+        res.sendStatus(401);
+    }
+}
+// server.use(auth);
+
+// USING DIFFERENT TYPES OF HTTPS MEHTODS
+server.get('/product/:id',(req,res)=>{
+    console.log(req.params);
+    res.json({type:'GET'});
+})
+server.post('/',auth,(req,res)=>{
+    res.json({type:'POST'});
+})
+server.put('/',(req,res)=>{
+    res.json({type:'PUT'});
+})
+server.patch('/',(req,res)=>{
+    res.json({type:'PATCH'});
+})
+server.delete('/',(req,res)=>{
+    res.json({type:'DELETE'});
+})
+
+server.listen(8080,()=>{
+    console.log("server started at 8080");
+})
